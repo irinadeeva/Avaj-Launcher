41968df832acc09f03666e62ed75f9c8ab64af3c
irinadeeva <dee.irina.a@mail.com>
2021-04-19 21:31:48 +0300

    new class

---
+++ src/com/company/Scenario.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class Scenario {
+    int timesToSimulationRun;
+
+    List<AircraftData> aircraftDataList = new ArrayList<AircraftData>();
+}
--- src/com/company/Simulator.java
+++ src/com/company/Simulator.java
@@ -1,14 +1,32 @@
 package com.company;
 
 public class Simulator {
+    public Scenario scenario;
 
     public static void main(String[] args) {
         if (args.length == 1 && args[0].endsWith(".txt")) {
+            readFile(args[0]);
             System.out.println("all right");
+        } else if (args.length == 0){
+            System.out.println("The program expects a text file that will contain the scenario that needs to be simulated");
         } else {
             System.out.println("The program takes only one argument");
         }
     }
+
+    public void readFile(String fileName)
+    {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName)))
+        {
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null)
+            {
+                System.out.println(sCurrentLine);
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
-
-
